ef25fd9ec6bcd5809c15520aa0d7938c66afdf24
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-06-14 16:44:23 +0700

    fpbs

--- src/fakultas.js
+++ src/fakultas.js
@@ -1,4 +1,3 @@
-
 const fakultas = [
   {
     slug: 'fpmipa',
@@ -295,6 +294,133 @@ const fakultas = [
     name: 'FPBS',
     slug: 'fpbs',
     url_image: 'http://165.22.109.98:9999/img/FPBS.jpg',
+    full_name: 'Fakultas Pendidikan Bahasa dan Sastra',
+    gallery: [
+      'http://165.22.109.98:9999/img/jepang1.jpg',
+      'http://165.22.109.98:9999/img/jepang4.jpg',
+      'http://165.22.109.98:9999/img/indo1.jpg',
+      'http://165.22.109.98:9999/img/inggris.jpg',
+      'http://165.22.109.98:9999/img/jepang.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+    prodi: [
+      {
+        name: 'Pendidikan Bahasa dan Sastra Indonesia',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/indo1.jpg',
+        slug: 's1-pendidikan-bahasa-dan-sastra-indonesia',
+      },
+      {
+        name: 'Pendidikan Bahasa Inggris',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/inggris.jpg',
+        slug: 's1-pendidikan-bahasa-inggris',
+      },
+      {
+        name: 'Pendidikan Bahasa Jepang',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/jepang1.jpg',
+        slug: 's1-pendidikan-bahasa-jepang',
+      },
+    ],
   },
   {
     name: 'FPSD',
@@ -588,7 +714,7 @@ const fakultas = [
         url_image: 'http://165.22.109.98:9999/img/Purwakarta7.jpg',
         slug: 's1-pendidikan-dan-teknologi-informasi',
       },
-    ]
+    ],
   },
   {
     name: 'Serang',
