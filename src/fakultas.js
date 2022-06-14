73fe160e15e7996ea2225c4a8a53bb313c957c72
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-06-14 16:47:32 +0700

    fpeb tasik

--- src/fakultas.js
+++ src/fakultas.js
@@ -694,12 +694,266 @@ const fakultas = [
   {
     name: 'FPEB',
     slug: 'fpeb',
-    url_image: 'http://165.22.109.98:9999/img/FPEB.jpeg',
+    url_image: 'http://165.22.109.98:9999/img/FPEB.jpg',
+    full_name: 'Fakultas Pendidikan Ekonomi dan Bisnis',
+    gallery: [
+      'http://165.22.109.98:9999/img/akuntansi1.jpg',
+      'http://165.22.109.98:9999/img/akuntansi2.jpg',
+      'http://165.22.109.98:9999/img/kantor1.jpg',
+      'http://165.22.109.98:9999/img/ekonomi1.jpg',
+      'http://165.22.109.98:9999/img/ekonomi2.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+    prodi: [
+      {
+        name: 'Pendidikan Akuntansi',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/akuntansi1.jpg',
+        slug: 's1-pendidikan-akuntansi',
+      },
+      {
+        name: 'Pendidikan Manajemen Perkantoran',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/kantor1.jpg',
+        slug: 's1-pendidikan-manajemen-perkantoran',
+      },
+      {
+        name: 'Ilmu Ekonomi dan Keuangan Islam',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/ekonomi1.jpg',
+        slug: 's1-ilmu-ekonomi-dan-keuangan-islam',
+      },
+    ],
   },
   {
-    name: 'Tasikmalaya',
+    name: 'KAMDA TASIKMALAYA',
     slug: 'tasikmalaya',
-    url_image: 'http://165.22.109.98:9999/img/Tasikmalaya.jpg',
+    url_image: 'http://165.22.109.98:9999/img/tasik.jpg',
+    full_name: 'Kamda Tasikmalaya',
+    gallery: [
+      'http://165.22.109.98:9999/img/kwu1.jpg',
+      'http://165.22.109.98:9999/img/kwu2.jpg',
+      'http://165.22.109.98:9999/img/ppl3.jpg',
+      'http://165.22.109.98:9999/img/ppl2.jpg',
+      'http://165.22.109.98:9999/img/desain1.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+    prodi: [
+      {
+        name: 'Pendidikan Guru Sekolah Dasar',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/ppl3.jpg',
+        slug: 's1-pendidikan-guru-sekolah-dasar',
+      },
+      {
+        name: 'Kewirausahaan',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/kwu1.jpg',
+        slug: 's1-kewirausahaan',
+      },
+      {
+        name: 'Desain Produk Industri',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/desain1.jpg',
+        slug: 's1-desain-produk-industri',
+      },
+    ],
   },
   {
     name: 'Cibiru',
