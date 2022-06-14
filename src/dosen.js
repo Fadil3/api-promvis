628ae50b394f417cba332ffbfbfdc15e6fdff385
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-06-14 14:44:58 +0700

    Update dosen.js

--- src/dosen.js
+++ src/dosen.js
@@ -1,52 +1,53 @@
 const dosen = [
-    {
-        nama:"Prof. Turmudi, M.Ed., M.Sc., Ph.D.",
-        jabatan:"Guru Besar",
-        golongan:"IV/d",
-    },
-    {
-        nama: "Suprih Widodo, S.Si., M.T.",
-        jabatan: "Lektor",
-        golongan: "III/c",
-    },
-    {
-        nama: "Suprih Widodo, S.Si., M.T.",
-        jabatan: "Lektor",
-        golongan: "III/c",
-    },
-    {
-        nama: "Suprih Widodo, S.Si., M.T.",
-        jabatan: "Lektor",
-        golongan: "III/c",
-    },
-    {
-        nama: "Suprih Widodo, S.Si., M.T.",
-        jabatan: "Lektor",
-        golongan: "III/c",
-    },
-    {
-        nama: "Suprih Widodo, S.Si., M.T.",
-        jabatan: "Lektor",
-        golongan: "III/c",
-    },
-    {
-        nama: "Suprih Widodo, S.Si., M.T.",
-        jabatan: "Lektor",
-        golongan: "III/c",
-    },
-    {
-        nama: "Suprih Widodo, S.Si., M.T.",
-        jabatan: "Lektor",
-        golongan: "III/c",
-    },
-    {
-        nama: "Suprih Widodo, S.Si., M.T.",
-        jabatan: "Lektor",
-        golongan: "III/c",
-    },
-    {
-        nama: "Suprih Widodo, S.Si., M.T.",
-        jabatan: "Lektor",
-        golongan: "III/c",
-    }
+  {
+    nama: 'Prof. Turmudi, M.Ed., M.Sc., Ph.D.',
+    jabatan: 'Guru Besar',
+    golongan: 'IV/d',
+  },
+  {
+    nama: 'Suprih Widodo, S.Si., M.T.',
+    jabatan: 'Lektor',
+    golongan: 'III/c',
+  },
+  {
+    nama: 'Suprih Widodo, S.Si., M.T.',
+    jabatan: 'Lektor',
+    golongan: 'III/c',
+  },
+  {
+    nama: 'Suprih Widodo, S.Si., M.T.',
+    jabatan: 'Lektor',
+    golongan: 'III/c',
+  },
+  {
+    nama: 'Suprih Widodo, S.Si., M.T.',
+    jabatan: 'Lektor',
+    golongan: 'III/c',
+  },
+  {
+    nama: 'Suprih Widodo, S.Si., M.T.',
+    jabatan: 'Lektor',
+    golongan: 'III/c',
+  },
+  {
+    nama: 'Suprih Widodo, S.Si., M.T.',
+    jabatan: 'Lektor',
+    golongan: 'III/c',
+  },
+  {
+    nama: 'Suprih Widodo, S.Si., M.T.',
+    jabatan: 'Lektor',
+    golongan: 'III/c',
+  },
+  {
+    nama: 'Suprih Widodo, S.Si., M.T.',
+    jabatan: 'Lektor',
+    golongan: 'III/c',
+  },
+  {
+    nama: 'Suprih Widodo, S.Si., M.T.',
+    jabatan: 'Lektor',
+    golongan: 'III/c',
+  },
 ]
+module.exports = dosen
