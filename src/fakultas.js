5bbbb2f93d56bb538b07dfc3d82dfa2c9e28eb0d
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-06-14 16:46:00 +0700

    fpsd

--- src/fakultas.js
+++ src/fakultas.js
@@ -426,6 +426,133 @@ const fakultas = [
     name: 'FPSD',
     slug: 'fpsd',
     url_image: 'http://165.22.109.98:9999/img/FPSD.jpg',
+    full_name: 'Fakultas Pendidikan Seni dan Desain',
+    gallery: [
+      'http://165.22.109.98:9999/img/senimusik.jpg',
+      'http://165.22.109.98:9999/img/senitari.jpg',
+      'http://165.22.109.98:9999/img/senirupa.jpg',
+      'http://165.22.109.98:9999/img/fpsd1.jpg',
+      'http://165.22.109.98:9999/img/fpsd2.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+    prodi: [
+      {
+        name: 'Pendidikan Seni Rupa dan Kerajinan',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/senirupa.jpg',
+        slug: 's1-pendidikan-seni-rupa-dan-kerajinan',
+      },
+      {
+        name: 'Pendidikan Seni Tari',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/senitari.jpg',
+        slug: 's1-pendidikan-seni-tari',
+      },
+      {
+        name: 'Pendidikan Seni Musik',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/senimusik.jpg',
+        slug: 's1-pendidikan-seni-musik',
+      },
+    ],
   },
   {
     name: 'FPTK',
