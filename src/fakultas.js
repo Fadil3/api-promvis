28ce7785aff163e518bfde2a8e5fb9e97e03020a
bimantoroar <75794187+bimantoroar@users.noreply.github.com>
2022-06-14 09:11:18 +0700

    FPIPS

--- src/fakultas.js
+++ src/fakultas.js
@@ -218,6 +218,134 @@ const fakultas = [
     name: 'FPIPS',
     slug: 'fpips',
     url_image: 'http://165.22.109.98:9999/img/FPIPS.jpg',
+    full_name: 'Fakultas Pendidikan Ilmu Pengetahuan Sosial',
+    gallery: [
+      'http://165.22.109.98:9999/img/FPIPS.jpg',
+      'http://165.22.109.98:9999/img/SEJARAH1.jpg',
+      'http://165.22.109.98:9999/img/SEJARAH2.jpg',
+      'http://165.22.109.98:9999/img/GEOGRAFI2.jpg',
+      'http://165.22.109.98:9999/img/GEOGRAFI3.jpg',
+      'http://165.22.109.98:9999/img/SOSIOLOGI2.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+    prodi: [
+      {
+        name: 'Pendidikan Sejarah',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/FPIPS.jpg',
+        slug: 's1-pendidikan-sejarah',
+      },
+      {
+        name: 'Pendidikan Geografi',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/FPIPS.jpg',
+        slug: 's1-pendidikan-geografi',
+      },
+      {
+        name: 'Pendidikan Sosiologi',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/FPIPS.jpg',
+        slug: 's1-pendidikan-sosiologi',
+      },
+    ],
   },
   {
     name: 'FPBS',
--- src/prodi.js
+++ src/prodi.js
@@ -144,6 +144,393 @@ const prodi = [
       },
     ],
   },
+  {
+    name: 'Pendidikan Sejarah',
+    jenjang: 'S1',
+    akreditasi: 'A',
+    url_image: 'http://165.22.109.98:9999/img/FPIPS.jpg',
+    slug: 's1-pendidikan-ilmu-sejarah',
+    gallery: [
+      'http://165.22.109.98:9999/img/SEJARAH1.jpg',
+      'http://165.22.109.98:9999/img/FPIPS.jpg',
+      'http://165.22.109.98:9999/img/SEJARAH2.jpg',
+      'http://165.22.109.98:9999/img/SEJARAH3.jpg',
+      'http://165.22.109.98:9999/img/SEJARAH4.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    description:
+      'The Department of History Education was founded on Wednesday, October 20, 1954, is one of the oldest institutions within the Indonesian Education University (UPI), and was directly led by Mohammad Yamin, who at that time served as Menetri PP and K (Teaching, Education, and Culture). ). This figure, in UPI’s academic circles, is also seen as one of the founding fathers of the PTPG (Teacher Education College), the forerunner of the UPI institution in Bandung.',
+    kurikulum: [
+      {
+        name: ' Local History',
+        sks: '3',
+        semester: '2',
+      },
+      {
+        name: 'Public History',
+        sks: '3',
+        semester: '5',
+      },
+      {
+        name: 'Historiography',
+        sks: '3',
+        semester: '4',
+      },
+    ],
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Pendidikan Geografi',
+    jenjang: 'S1',
+    akreditasi: 'A',
+    url_image: 'http://165.22.109.98:9999/img/FPIPS.jpg',
+    slug: 's1-pendidikan-ilmu-geografi',
+    gallery: [
+      'http://165.22.109.98:9999/img/GEOGRAFI1.jpg',
+      'http://165.22.109.98:9999/img/GEOGRAFI2.jpg',
+      'http://165.22.109.98:9999/img/GEOGRAFI3.jpg',
+      'http://165.22.109.98:9999/img/GEOGRAFI4.jpg',
+      'http://165.22.109.98:9999/img/GEOGRAFI5.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    description:
+      'Departemen pendidikan geografi berdiri sejak tahun 1961 berdasarkan SK menteri P & K nomor 06/MPK/161. Berdasarkan SK Presiden RI No.124 tanggal 7 oktober 1999, Jurusan Pendidikan Geografi bertugas untuk menghasikan calon tenaga pendidik. Selama kurun waktu tersebut, jurusan telah banyak berkiprah dalam menghasilkan lulusan, menjadi mitra lembaga pendidikan tenaga kependidikan (LPTK) dan perguruan tinggi lainnya dalam kegiatan ilmiah, serta melaksanakan pengabdian pada masyarakat. Kerjasama dan kemitraan tersebut bertujuan untuk meningkatkan kualitas lulusan, pengembangan disiplin  ilmu dan aplikasinya. Kegiatan tersebut, merupakan misi pada aspek  penyelenggaraan pendidikan, penelitian, dan pengabdian kepada masyarakat.',
+    kurikulum: [
+      {
+        name: 'GEOMORFOLOGI',
+        sks: '3',
+        semester: '2',
+      },
+      {
+        name: 'OCEANOGRAFI',
+        sks: '3',
+        semester: '2',
+      },
+      {
+        name: 'ANTROPOLOGI',
+        sks: '3',
+        semester: '3',
+      },
+    ],
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Pendidikan Sosiologi',
+    jenjang: 'S1',
+    akreditasi: 'A',
+    url_image: 'http://165.22.109.98:9999/img/FPIPS.jpg',
+    slug: 's1-pendidikan-ilmu-sosiologi',
+    gallery: [
+      'http://165.22.109.98:9999/img/SOSIOLOGI1.jpg',
+      'http://165.22.109.98:9999/img/SOSIOLOGI2.jpg',
+      'http://165.22.109.98:9999/img/SOSIOLOGI3.jpg',
+      'http://165.22.109.98:9999/img/SOSIOLOGI4.jpg',
+      'http://165.22.109.98:9999/img/SOSIOLOGI5.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    description:
+      'Pendirian Prodi Pendidikan Sosiologi didasari upaya untuk memenuhi kebutuhan guru profesional dalam mata pelajaran Sosiologi yang menurut hasil studi kelayakan ditemukan bahwa pembelajaran Sosiologi di tingkat persekolahan selama ini tidak diampu oleh guru-guru yang berlatar belakang Pendidikan Sosiologi atau guru tersebut tidak menempuh pendidikan tinggi di Jurusan atau Program Studi Pendidikan Sosiologi.',
+    kurikulum: [
+      {
+        name: 'SOSIOLOGI KOMUNIKASI',
+        sks: '3',
+        semester: '2',
+      },
+      {
+        name: 'DEMOGRAFI SOSIAL',
+        sks: '2',
+        semester: '2',
+      },
+      {
+        name: 'PSIKOLOGI SOSIAL ',
+        sks: '2',
+        semester: '4',
+      },
+    ],
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+  },
 ]
 
 module.exports = prodi
