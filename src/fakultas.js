a86a425c45b4498f4c793739d8693d0371e066a6
Diffa Al Farrisztqi <diffafarriz@gmail.com>
2022-06-14 10:42:21 +0700

    Menambahkan data FIP FPOK Kamda Purwakarta
    Menambahkan data fakultas dan prodi yang ada di FIP, FPOK, dan Kamda Purwakarta

--- src/fakultas.js
+++ src/fakultas.js
@@ -160,57 +160,129 @@ const fakultas = [
     url_image: 'http://165.22.109.98:9999/img/FIP.png',
     full_name: 'Fakultas Ilmu Pendidikan',
     gallery: [
-      'http://165.22.109.98:9999/img/FIP.png',
-      'http://165.22.109.98:9999/img/FIP.png',
-      'http://165.22.109.98:9999/img/FIP.png',
-      'http://165.22.109.98:9999/img/FIP.png',
-      'http://165.22.109.98:9999/img/FIP.png',
-      'http://165.22.109.98:9999/img/FIP.png',
+      'http://165.22.109.98:9999/img/FIP1.jpeg',
+      'http://165.22.109.98:9999/img/FIP2.jpg',
+      'http://165.22.109.98:9999/img/FIP3.jpg',
+      'http://165.22.109.98:9999/img/FIP4.jpg',
+      'http://165.22.109.98:9999/img/FIP5.jpeg',
     ],
     ratio: '1:30',
     avg_study_time: '3.5',
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
     prodi: [
       {
-        name: 'Pendidikan Matematika',
+        name: 'Bimbingan dan Konseling',
         jenjang: 'S1',
         akreditasi: 'A',
-        url_image: 'http://165.22.109.98:9999/img/FIP.png',
-        slug: 's1-pendidikan-matematika',
+        url_image: 'http://165.22.109.98:9999/img/FIP6.png',
+        slug: 's1-bimbingan-konseling',
       },
       {
-        name: 'Pendidikan Biologi',
+        name: 'Administrasi Pendidikan',
         jenjang: 'S1',
         akreditasi: 'A',
-        url_image: 'http://165.22.109.98:9999/img/FIP.png',
-        slug: 's1-pendidikan-biologi',
+        url_image: 'http://165.22.109.98:9999/img/FIP7.png',
+        slug: 's1-administrasi-pendidikan',
       },
       {
-        name: 'Pendidikan Kimia',
+        name: 'Pendidikan Luar Sekolah',
         jenjang: 'S1',
         akreditasi: 'A',
-        url_image: 'http://165.22.109.98:9999/img/FIP.png',
-        slug: 's1-pendidikan-kimia',
-      },
-      {
-        name: 'Pendidikan Fisika',
-        jenjang: 'S1',
-        akreditasi: 'A',
-        url_image: 'http://165.22.109.98:9999/img/FIP.png',
-        slug: 's1-pendidikan-fisika',
-      },
-      {
-        name: 'Pendidikan Ilmu Komputer',
-        jenjang: 'S1',
-        akreditasi: 'A',
-        url_image: 'http://165.22.109.98:9999/img/FIP.png',
-        slug: 's1-pendidikan-ilmu-komputer',
-      },
-      {
-        name: 'International Program on Science Education',
-        jenjang: 'S1',
-        akreditasi: 'A',
-        url_image: 'http://165.22.109.98:9999/img/FIP.png',
-        slug: 's1-international-program-on-science-education',
+        url_image: 'http://165.22.109.98:9999/img/FIP8.jpeg',
+        slug: 's1-luar-sekolah',
       },
     ],
   },
@@ -238,6 +310,133 @@ const fakultas = [
     name: 'FPOK',
     slug: 'fpok',
     url_image: 'http://165.22.109.98:9999/img/FPOK.jpg',
+    full_name: 'Fakultas Pendidikan Olahraga dan Kesehatan',
+    gallery: [
+      'http://165.22.109.98:9999/img/FPOK1.jpg',
+      'http://165.22.109.98:9999/img/FPOK2.jpg',
+      'http://165.22.109.98:9999/img/FPOK3.jpeg',
+      'http://165.22.109.98:9999/img/FPOK4.jpg',
+      'http://165.22.109.98:9999/img/FPOK5.jpeg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+    prodi: [
+      {
+        name: 'Pendidikan Kepelatihan Olahraga',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/FPOK5.jpeg',
+        slug: 's1-pendidikan-kepelatihan-olahraga',
+      },
+      {
+        name: 'Pendidikan Kesehatan dan Rekreasi',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/FPOK4.jpg',
+        slug: 's1-pendidikan-kesehatan-dan-rekreasi',
+      },
+      {
+        name: 'Pendidikan Jasmani, Kesehatan dan Rekreasi',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/FPOK6.png',
+        slug: 's1-pendidikan-jasmani-kesehatan-dan-rekreasi',
+      },
+    ],
   },
   {
     name: 'FPEB',
@@ -260,9 +459,136 @@ const fakultas = [
     url_image: 'http://165.22.109.98:9999/img/Sumedang.jpg',
   },
   {
-    name: 'Purwakarta',
+    name: 'KAMDA PURWAKARTA',
     slug: 'purwakarta',
     url_image: 'http://165.22.109.98:9999/img/Purwakarta.jpg',
+    full_name: 'Purwakarta',
+    gallery: [
+      'http://165.22.109.98:9999/img/Purwakarta0.png',
+      'http://165.22.109.98:9999/img/Purwakarta1.jpg',
+      'http://165.22.109.98:9999/img/Purwakarta2.png',
+      'http://165.22.109.98:9999/img/Purwakarta3.jpeg',
+      'http://165.22.109.98:9999/img/Purwakarta4.jpeg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+    prodi: [
+      {
+        name: 'Pendidikan Guru Sekolah Dasar',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/Purwakarta5.jpeg',
+        slug: 's1-pendidikan-guru-sekolah-dasar',
+      },
+      {
+        name: 'Pendidikan Guru Pendidikan Anak Usia Dini',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/Purwakarta6.jpg',
+        slug: 's1-pendidikan-guru-pendidikan-anak-usia-dini',
+      },
+      {
+        name: 'Pendidikan dan Teknologi Informasi',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'http://165.22.109.98:9999/img/Purwakarta7.jpg',
+        slug: 's1-pendidikan-dan-teknologi-informasi',
+      },
+    ]
   },
   {
     name: 'Serang',
