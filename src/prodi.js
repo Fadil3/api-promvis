2e147a4329ec57ea6abe3a3717d958a31605649c
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-06-15 20:37:28 +0700

    fadli fix pendidikan akuntansi

--- src/prodi.js
+++ src/prodi.js
@@ -968,7 +968,7 @@ const prodi = [
     jenjang: 'S1',
     akreditasi: 'A',
     url_image: 'http://165.22.109.98:9999/img/ppl3.jpg',
-    slug: 's1-pendidikan-akuntansi',
+    slug: 's1-pendidikan-guru-sekolah-dasar-kamda-tasikmalaya',
     gallery: [
       'http://165.22.109.98:9999/img/ppl1.jpg',
       'http://165.22.109.98:9999/img/ppl2.jpg',
