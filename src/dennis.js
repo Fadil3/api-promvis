463c86218a858d5568501d223e6eea5b9400c5da
dennisnuriman <dennismuhammad.md@gmail.com>
2022-06-15 16:40:09 +0700

    prodi logistik dan kelautan

--- src/dennis.js
+++ src/dennis.js
@@ -252,6 +252,13 @@ const fakultas = [
         url_image: "http://165.22.109.98:9999/img/FIP6.png",
         slug: "s1-pendidikan-kelautan-dan-perikanan",
       },
+      {
+        name: "Logistik Kelautan",
+        jenjang: "S1",
+        akreditasi: "A",
+        url_image: "http://165.22.109.98:9999/img/FIP6.png",
+        slug: "s1-logistik-kelautan",
+      },
     ],
   },
 ];
