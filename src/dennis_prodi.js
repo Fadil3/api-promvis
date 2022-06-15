fd7fdcfa9a7d87f6a07acde23c5c450e0743891e
dennisnuriman <dennismuhammad.md@gmail.com>
2022-06-15 15:46:00 +0700

    prodi keperawatan

--- src/dennis_prodi.js
+++ src/dennis_prodi.js
@@ -149,4 +149,154 @@ const prodi = [
       },
     ],
   },
+  {
+    name: "Keperawatan",
+    jenjang: "S1",
+    akreditasi: "A",
+    url_image: "http://165.22.109.98:9999/img/kamda_sumedang_keperawatan.jpg",
+    slug: "s1-keperawatan",
+    gallery: [
+      "http://165.22.109.98:9999/img/kamda_sumedang_keperawatan.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_keperawatan.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_keperawatan.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_keperawatan.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_keperawatan.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_keperawatan.jpg",
+    ],
+    ratio: "1:30",
+    avg_study_time: "3.5",
+    description:
+      "Become leading and outstanding study program in the field of Computer Science Education at the national level and recognized at the ASEAN level",
+    kurikulum: [
+      {
+        name: "Gizi Olahraga",
+        sks: "2",
+        semester: "3",
+      },
+      {
+        name: "Fisioterapi Olahraga",
+        sks: "2",
+        semester: "3",
+      },
+      {
+        name: "Bahasa Inggris Keperawatan",
+        sks: "3",
+        semester: "3",
+      },
+      {
+        name: "Statistika",
+        sks: "3",
+        semester: "3",
+      },
+      {
+        name: "Keperawatan Medikal Bedah I",
+        sks: "3",
+        semester: "3",
+      },
+      {
+        name: "Praktik Keperawatan Dasar",
+        sks: "3",
+        semester: "3",
+      },
+      {
+        name: "Manajemen Keperawatan",
+        sks: "2",
+        semester: "3",
+      },
+    ],
+    stats: [
+      {
+        title: "Rasio Dosen Berdasarkan Gender",
+        data: [
+          {
+            tahun: "2021",
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: "2022",
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: "Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir",
+        data: [
+          {
+            tahun: "2021",
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: "2022",
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: "Keketatan Penerimaaan Mahasiswa",
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: "Rasio Dosen Berdasarkan Jabatan Fungsional",
+        data: [
+          {
+            tahun: "2020",
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: "2021",
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+  },
 ];
