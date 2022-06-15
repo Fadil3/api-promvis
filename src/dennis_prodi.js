126bef35e65926bd0087f8045e63d88acb28994b
dennisnuriman <dennismuhammad.md@gmail.com>
2022-06-15 15:51:55 +0700

    prodi industri pariwisata

--- src/dennis_prodi.js
+++ src/dennis_prodi.js
@@ -299,4 +299,154 @@ const prodi = [
       },
     ],
   },
+  {
+    name: "Industri Pariwisata",
+    jenjang: "S1",
+    akreditasi: "A",
+    url_image: "http://165.22.109.98:9999/img/kamda_sumedang_pariwisata.jpg",
+    slug: "s1-industri-pariwisata",
+    gallery: [
+      "http://165.22.109.98:9999/img/kamda_sumedang_pariwisata.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_pariwisata.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_pariwisata.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_pariwisata.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_pariwisata.jpg",
+      "http://165.22.109.98:9999/img/kamda_sumedang_pariwisata.jpg",
+    ],
+    ratio: "1:30",
+    avg_study_time: "3.5",
+    description:
+      "Become leading and outstanding study program in the field of Computer Science Education at the national level and recognized at the ASEAN level",
+    kurikulum: [
+      {
+        name: "Bahasa Asing",
+        sks: "2",
+        semester: "3",
+      },
+      {
+        name: "Studi Kelayakan Bisnis Hospitality",
+        sks: "3",
+        semester: "3",
+      },
+      {
+        name: "Pariwisata Internasional",
+        sks: "2",
+        semester: "3",
+      },
+      {
+        name: "Pariwisata Internasional",
+        sks: "2",
+        semester: "3",
+      },
+      {
+        name: "Operasionl Front Office",
+        sks: "3",
+        semester: "3",
+      },
+      {
+        name: "Pengetahuan Food & Beverge",
+        sks: "3",
+        semester: "3",
+      },
+      {
+        name: "MICE Management",
+        sks: "3",
+        semester: "3",
+      },
+    ],
+    stats: [
+      {
+        title: "Rasio Dosen Berdasarkan Gender",
+        data: [
+          {
+            tahun: "2021",
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: "2022",
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: "Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir",
+        data: [
+          {
+            tahun: "2021",
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: "2022",
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: "Keketatan Penerimaaan Mahasiswa",
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: "Rasio Dosen Berdasarkan Jabatan Fungsional",
+        data: [
+          {
+            tahun: "2020",
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: "2021",
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+  },
 ];
