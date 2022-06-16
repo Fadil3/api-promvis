3e32bc2f90224528534d3ed2b28c0fcbc5727f4a
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-06-16 21:13:36 +0700

    Update prodi.js

--- src/prodi.js
+++ src/prodi.js
@@ -4735,7 +4735,7 @@ const prodi = [
     name: 'Pendidikan Kepelatihan Olahraga',
     jenjang: 'S1',
     akreditasi: 'A',
-    url_image: 'http://165.22.109.98:9999/img/FPOK5.jpeg',
+    url_image: 'http://165.22.109.98:9999/img/FPOK5.jpg',
     slug: 's1-pendidikan-kepelatihan-olahraga',
     gallery: [
       'http://165.22.109.98:9999/img/PKO.jpg',
@@ -4922,6 +4922,428 @@ const prodi = [
         name: 'Sport Medicine',
         sks: '3',
         semester: '3',
+      },
+    ],
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Pendidikan Guru Pendidikan Anak Usia Dini',
+    jenjang: 'S1',
+    akreditasi: 'A',
+    url_image: 'http://165.22.109.98:9999/img/Purwakarta6.jpg',
+    slug: 's1-pendidikan-guru-pendidikan-anak-usia-dini',
+    gallery: [
+      'http://165.22.109.98:9999/img/pgpaud.png',
+      'http://165.22.109.98:9999/img/pgpaud1.jpg',
+      'http://165.22.109.98:9999/img/pgpaud2.png',
+      'http://165.22.109.98:9999/img/pgpaud3.png',
+      'http://165.22.109.98:9999/img/pgpaud4.png',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    description:
+      'Menghasilkan lulusan yang mampu melaksanakan, mengembangkan, dan mengelola pendidikan anak usia dini di lembaga Pendidikan Anak Usia Dini baik di Taman Kanak-Kanak (TK), Kelompok Bermain (KB), Tempat Penitipan Anak (TPA) dan Satuan PAUD Sejenis',
+    kurikulum: [
+      {
+        name: 'Landasan Pendidikan',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'Pembelajaran Atletik',
+        name: 'Psikologi Pendidikan',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'Anatomi dan Fisiologi Manusia',
+        name: 'Kurikulum dan Pembelajaran',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'Pembelajaran Permainan Bola Basket',
+        name: 'Pedagogik',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'DMP Aquatik',
+        name: 'Kebijakan dan Inovasi Pendidikan',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'DMP Atletik',
+        name: 'Pendidikan Inklusi',
+        sks: '3',
+        semester: '3',
+      },
+    ],
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Pendidikan dan Teknologi Informasi',
+    jenjang: 'S1',
+    akreditasi: 'A',
+    url_image: 'http://165.22.109.98:9999/img/Purwakarta7.jpg',
+    slug: 's1-pendidikan-dan-teknologi-informasi',
+    gallery: [
+      'http://165.22.109.98:9999/img/PTI.jpg',
+      'http://165.22.109.98:9999/img/PTI1.jpg',
+      'http://165.22.109.98:9999/img/PTI2.jpg',
+      'http://165.22.109.98:9999/img/PTI3.jpg',
+      'http://165.22.109.98:9999/img/PTI4.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    description:
+      'Menjadi Program Studi Pelopor dan Unggul dalam Pendidikan Sistem dan Teknologi Informasi di tingkat Nasional tahun 2020',
+    kurikulum: [
+      {
+        name: 'Kurikulum dan Pembelajaran',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'Pengelolaan Pendidikan',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'Strategi Pembelajaran Bidang Studi',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'Literasi ICT dan Media Pembelajaran Bidang Studi',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'Evaluasi Pembelajaran Bidang Studi',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'Perencanaan Pembelajaran Pembelajaran Bidang Studi',
+        sks: '3',
+        semester: '3',
+      },
+    ],
+    stats: [
+      {
+        title: 'Rasio Dosen Berdasarkan Gender',
+        data: [
+          {
+            tahun: '2021',
+            laki_laki: 38,
+            perempuan: 62,
+          },
+          {
+            tahun: '2022',
+            laki_laki: 40,
+            perempuan: 60,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir',
+        data: [
+          {
+            tahun: '2021',
+            s2: 53,
+            s3: 47,
+          },
+          {
+            tahun: '2022',
+            s2: 41,
+            s3: 59,
+          },
+        ],
+      },
+      {
+        title: 'Keketatan Penerimaaan Mahasiswa',
+        data: [
+          {
+            tahun: 2017,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2018,
+            snmptn: 29,
+            sbmptn: 29,
+            smupi: 11,
+          },
+          {
+            tahun: 2019,
+            snmptn: 18,
+            sbmptn: 17,
+            smupi: 7,
+          },
+          {
+            tahun: 2020,
+            snmptn: 18,
+            sbmptn: 11,
+            smupi: 7,
+          },
+          {
+            tahun: 2021,
+            snmptn: 20,
+            sbmptn: 12,
+            smupi: 8,
+          },
+          {
+            tahun: 2022,
+            snmptn: 18,
+            sbmptn: 10,
+            smupi: 4,
+          },
+        ],
+      },
+      {
+        title: 'Rasio Dosen Berdasarkan Jabatan Fungsional',
+        data: [
+          {
+            tahun: '2020',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+          {
+            tahun: '2021',
+            tp: 4,
+            aa: 8,
+            lk: 12,
+            lkp: 22,
+            gb: 5,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Pendidikan Bahasa Jepang',
+    jenjang: 'S1',
+    akreditasi: 'A',
+    url_image: 'http://165.22.109.98:9999/img/jepang1.jpg',
+    slug: 's1-pendidikan-bahasa-jepang',
+    gallery: [
+      'http://165.22.109.98:9999/img/jepang4.jpg',
+      'http://165.22.109.98:9999/img/jepang2.jpg',
+      'http://165.22.109.98:9999/img/jepang3.jpg',
+      'http://165.22.109.98:9999/img/jepang1.jpg',
+      'http://165.22.109.98:9999/img/jepang5.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    description:
+      'Departemen Pendidikan Bahasa Jepang Universitas Pendidikan Indonesia resmi dibuka pada tahun 1965, yang ditetapkan dengan Instruksi Menteri PTIP No. 5 tahun 1965 tentang Pelaksanaan Kurikulum IKIP Negeri. ',
+    kurikulum: [
+      {
+        name: 'Pendidikan Bahasa Jepang',
+        sks: '3',
+        semester: '3',
+      },
+      {
+        name: 'Pendidikan Bahasa Indonesia',
+        sks: '2',
+        semester: '3',
+      },
+      {
+        name: 'Pendidikan Bahasa Inggris',
+        sks: '2',
+        semester: '4',
       },
     ],
     stats: [
